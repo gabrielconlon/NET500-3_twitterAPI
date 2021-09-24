3b825dcdbfdc5963dad76c5e8e767243bebd06b1
Gabe <3859926+gratefulgabe@users.noreply.github.com>
2021-09-24 09:01:01 -0600

    working authentication with absolute filepaths for security (no more hardcoded keys)

--- twitterAPI.py
+++ twitterAPI.py
@@ -2,12 +2,20 @@
 
 import tweepy
 
+# import keys
+apiKey = open("/Users/gabrielconlon/.ssh/twitterAPIKeys/key.pub", "r").readline().strip()
+apiSecretKey = open("/Users/gabrielconlon/.ssh/twitterAPIKeys/key", "r").readline().strip()
+
+accessKey = open("/Users/gabrielconlon/.ssh/twitterAPIKeys/access.pub", "r").readline().strip()
+accessSecretKey = open("/Users/gabrielconlon/.ssh/twitterAPIKeys/access", "r").readline().strip()
+
+
 # Authenticate to Twitter
 #For the OAuthHandler, use the API key followed by the API secret Key. (From "Consumer Keys")
-auth = tweepy.OAuthHandler("0Gw6VR69N1Z7DS5yvKO1RLh1F", "pamyF8wbl7NCYjsrUpPNqMlVgIcvqRCjyIHX4FzQQNBfxH8N2o")
+auth = tweepy.OAuthHandler(apiKey, apiSecretKey)
 
 #For the set_access_token, use the access token followed by the access token secret (From "Authentication Tokens")
-auth.set_access_token("1369710522241675264-JQ72t6e5Znrhbjv8HLwkgmVc1xqcuJ", "FKT703PdVCLFlfVcy7lv88pgGYVeyEYSixJ9cRpbl3noU")
+auth.set_access_token(accessKey, accessSecretKey)
 
 api = tweepy.API(auth)
 if (api.verify_credentials()):
